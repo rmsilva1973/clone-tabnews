9f6726a75c56f6536e66a70a8dc6c9320be5f4fd
Renato Silva <rmsilva1973@users.noreply.github.com>
2024-10-27 19:45:01 +0000

    finish `/api/v1/status enoint`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  var result = await database.query("SHOW server_version");
+  const dbVersion = result.rows[0].server_version;
+
+  result = await database.query("SHOW max_connections;");
+  const maxConnections = result.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  result = await database.query({
+    text: "SELECT COUNT(*)::int AS current_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const openedConnections = result.rows[0].current_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      databases: {
+        version: dbVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
 
 export default status;
