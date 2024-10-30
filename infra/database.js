1d9077b1392264b0c1fc1eb528b1187ee8aa86a9
Renato Silva <rmsilva1973@users.noreply.github.com>
2024-10-30 11:28:18 +0000

    splashes database password on logs

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,15 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
+  database_creds = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
+  };
+  const client = new Client(database_creds);
+  console.log("Credenciais do banco:", database_creds);
   var result = null;
   try {
     await client.connect();
