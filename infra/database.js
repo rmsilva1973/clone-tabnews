d0eefb6cd1d9754b3cdda5e2e7507c0a169dd052
Renato Silva <rmsilva1973@users.noreply.github.com>
2024-10-31 02:00:18 +0000

    add a more robust SSL configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,10 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   };
   const client = new Client(database_creds);
-  console.log("Credenciais do banco:", database_creds);
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -23,6 +22,14 @@ async function query(queryObject) {
   }
 }
 
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+}
 export default {
   query: query,
 };
